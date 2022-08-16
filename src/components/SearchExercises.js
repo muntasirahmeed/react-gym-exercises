24a1a551d39a90a88cfb3e8e1781825bffb0f9c8
Muntasir Ahmed <muntasirahmed@Muntasirs-MacBook-Air.local>
2022-08-16 14:43:44 +0600

    exercise heading design done

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import React from "react";
 
 const SearchExercises = () => {
-    return <div>SearchExercises</div>;
+  return (
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+          <Typography fontWeight={700} sx={{
+          fontSize:{lg:'44px', xs:'30px'}
+          }}
+          mb='50px' textAlign={'center'}>
+        Awesome Exercises you <br />
+        Should Know
+      </Typography>
+    </Stack>
+  );
 };
 
 export default SearchExercises;
