6a16a5725ba078241abaa9f73448cae08fc6cc50
Muntasir Ahmed <muntasirahmed@Muntasirs-MacBook-Air.local>
2022-08-16 14:04:59 +0600

    navigation link crated and setup done

--- src/components/Navber.js
+++ src/components/Navber.js
@@ -5,7 +5,16 @@ import { Link } from "react-router-dom";
 import Logo from "../assets/images/Logo.png";
 const Navber = () => {
   return (
-      <Stack direction="row" justifyContent='space-around' sx={{ gap: { sm: '122px', xs: '40px' }, mt: { sm: '32px', xs:'20px'}}}>
+    <Stack
+      direction="row"
+      justifyContent="space-around"
+      sx={{
+        gap: { sm: "122px", xs: "40px" },
+        mt: { sm: "32px", xs: "20px" },
+        justifyContent: "none",
+      }}
+      px="20px"
+    >
       <Link to="/">
         <img
           src={Logo}
