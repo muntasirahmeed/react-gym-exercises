daa1d87e1137ede8c53bd757f4eb415efe7d3193
Muntasir Ahmed <muntasirahmed@Muntasirs-MacBook-Air.local>
2022-08-18 02:21:47 +0600

    footer done

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Stack, Typography } from '@mui/material';
-import Icon from '../assets/icons/gym.png';
+import React from "react";
+import { Stack, Typography } from "@mui/material";
+import Icon from "../assets/icons/gym.png";
 
 const BodyPart = ({ item, setBodyPart, bodyPart }) => (
   <Stack
@@ -17,10 +17,10 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => (
       cursor: "pointer",
       gap: "47px",
     }}
-    // onClick={() => {
-    //   setBodyPart(item);
-    //   window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
-    // }}
+    onClick={() => {
+      setBodyPart(item);
+      window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
+    }}
   >
     <img src={Icon} alt="dumbbell" style={{ width: "40px", height: "40px" }} />
     <Typography
@@ -37,4 +37,12 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => (
 );
 
 export default BodyPart;
-
+// sx={{
+//   borderTop: bodyPart === item ? "4px solid #FF2625" : "",
+//   backgroundColor: "#fff",
+//   borderBottomLeftRadius: "20px",
+//   width: "270px",
+//   height: "282px",
+//   cursor: "pointer",
+//   gap: "47px",
+// }}
--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,39 +1,48 @@
-import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import { fetchData, exerciseOptions } from "../utils/fetchData";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+
+import { exerciseOptions, fetchData } from "../utils/fetchData";
 import HorizontalScrollbar from "./HorizontalScrollbar";
 
 const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   const [search, setSearch] = useState("");
   const [bodyParts, setBodyParts] = useState([]);
+
   useEffect(() => {
     const fetchExercisesData = async () => {
       const bodyPartsData = await fetchData(
         "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
         exerciseOptions
       );
+
       setBodyParts(["all", ...bodyPartsData]);
     };
+
     fetchExercisesData();
   }, []);
 
   const handleSearch = async () => {
     if (search) {
-      const exerciseData = await fetchData(
+      const exercisesData = await fetchData(
         "https://exercisedb.p.rapidapi.com/exercises",
         exerciseOptions
       );
-      const searchedExercises = exerciseData.filter(
-        (exercise) =>
-          exercise.name.toLowerCase().includes(search) ||
-          exercise.target.toLowerCase().includes(search) ||
-          // exercise.equipment.toLowerCase().includes(search) ||
-          exercise.bodyPart.toLowerCase().includes(search)
+
+      const searchedExercises = exercisesData.filter(
+        (item) =>
+          item.name.toLowerCase().includes(search) ||
+          item.target.toLowerCase().includes(search) ||
+          item.equipment.toLowerCase().includes(search) ||
+          item.bodyPart.toLowerCase().includes(search)
       );
+
+      window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
+
       setSearch("");
       setExercises(searchedExercises);
     }
   };
+
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
       <Typography
@@ -78,8 +87,9 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
       <Box sx={{ position: "relative", width: "100%", p: "20px" }}>
         <HorizontalScrollbar
           data={bodyParts}
-          bodyPart={bodyPart}
+          bodyParts
           setBodyPart={setBodyPart}
+          bodyPart={bodyPart}
         />
       </Box>
     </Stack>
